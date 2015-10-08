76c7080b953ef47c174971987986e22b78abc639
Andres Lamont <alamont@gmail.com>
2015-10-08 17:47:24 +0200

    Update docker-entrypoint-override.sh

--- docker-entrypoint-override.sh
+++ docker-entrypoint-override.sh
@@ -5,4 +5,7 @@ set -e
 echo "adding admin user..."
 esusers useradd $ES_USER -p $ES_PASS -r admin
 
+chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+exec gosu elasticsearch "$@"
+
 exec /docker-entrypoint.sh $@
